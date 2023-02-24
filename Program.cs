8086245f7ba53f03648e19812e3b6acee4930580
Alex Wigforss <alex.wigforss@gmail.com>
2023-02-24 09:17:20 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace IDP_assg_3
+{
+    internal class Program
+    {
+        class Grundämne
+        {
+            public string? namn;
+            public int atomnummer;
+            public string? typ;
+            public double smältpunkt;
+            public double kokpunkt;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
